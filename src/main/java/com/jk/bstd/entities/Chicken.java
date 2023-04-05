1c9481e40714c5f3f0ad652beb4b490344f1b2ae
kirayoon <113317993+kirayoon@users.noreply.github.com>
2023-04-05 14:24:23 -0700

    Add temporary function

--- src/main/java/com/jk/bstd/entities/Chicken.java
+++ src/main/java/com/jk/bstd/entities/Chicken.java
@@ -1,16 +1,33 @@
 package com.jk.bstd.entities;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.util.Objects;
+
 public class Chicken extends Animal {
     public static final int HEALTH = 30;
     public static final int SPEED = 4;
     public static final int ATTACK = 10;
     public static final int GOLD_DROPPED = 15;
+    public ImageView imgView;
+    public Image img;
+
     public Chicken() {
         // TODO: Change spawn point
+        // TODO: Change image
         super(HEALTH, SPEED, ATTACK, GOLD_DROPPED);
+        img = new Image(Objects.requireNonNull(getClass()
+                .getResource("/images/gameScreen/Farmer.png")).toExternalForm());
+        imgView = new ImageView(img);
     }
     // TODO: Implement move method
     // chicken should move 1 space per 1 unit of time
     @Override
     public void move() {}
+    // TODO: fix get imgview
+    @Override
+    public ImageView getImgView() {
+        return imgView;
+    }
 }
