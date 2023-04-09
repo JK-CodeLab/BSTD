eddf8621c6deaf6a09c5f88b64eb6f97d4c39f90
Joseph Chun <83483639+Mrrm2@users.noreply.github.com>
2023-04-08 22:21:36 -0700

    Add javadoc

--- src/main/java/com/jk/bstd/components/ShopButton.java
+++ src/main/java/com/jk/bstd/components/ShopButton.java
@@ -11,20 +11,35 @@ import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
 
+import java.util.Objects;
 
-
+/**
+ * Class that represents a button in the shop.
+ *
+ * @author Joseph Chun, Kira Yoon
+ * @version 1.0
+ */
 public class ShopButton extends Button {
+    private static final int BEGINNING_INDEX = 4;
+    private static final int OFFSET_X = 32;
+    private static final int OFFSET_Y = 32;
+    private static final int HEIGHT = 64;
+    private static final int Y_MODIFIER = 3;
     private static final String STYLE = "-fx-background-color: transparent;";
     private final String imgName;
     private final String itemName;
-
     private Image shopBtnImg;
     private Image itemImg;
 
-    public ShopButton(String imgName) {
+    /**
+     * Constructor for a new shop button.
+     *
+     * @param imgName the name of the image
+     */
+    public ShopButton(final String imgName) {
         this.imgName = imgName;
 
-        this.itemName = imgName.substring(4);
+        this.itemName = imgName.substring(BEGINNING_INDEX);
         initializeButtonListeners(this);
 
         setImagePath();
@@ -34,11 +49,16 @@ public class ShopButton extends Button {
     }
 
     private void setImagePath() {
-        shopBtnImg = new Image(getClass().getResource("/images/gameScreen/" + imgName + ".png").toExternalForm());
-        itemImg = new Image(getClass().getResource("/images/gameScreen/" + itemName + ".png").toExternalForm());
+        shopBtnImg = new Image(
+                Objects.requireNonNull(getClass().getResource("/images/gameScreen/" + imgName + ".png")
+                ).toExternalForm()
+        );
+        itemImg = new Image(
+                Objects.requireNonNull(getClass().getResource("/images/gameScreen/" + itemName + ".png")
+                ).toExternalForm()
+        );
     }
-
-    private void initializeButtonListeners(Node node) {
+    private void initializeButtonListeners(final Node node) {
         node.setOnDragDetected(event -> {
             Dragboard db = startDragAndDrop(TransferMode.COPY);
             ClipboardContent content = new ClipboardContent();
@@ -47,27 +67,26 @@ public class ShopButton extends Button {
 
             ImageView iv = new ImageView(itemImg);
             boolean tileOrSprinkler = itemName.equals("Tile") || itemName.equals("Sprinkler");
-            double offsetX, offsetY;
+            double offsetX = OFFSET_X;
+            double offsetY = OFFSET_Y;
             if (OSPlatform.isWindows()) {
-                offsetX = 32;
-                offsetY = 32;
                 if (!tileOrSprinkler) {
-                    offsetY *= 3;
+                    offsetY *= Y_MODIFIER;
                 }
             } else {
                 offsetX = 0;
                 offsetY = 0;
                 if (!tileOrSprinkler) {
-                    offsetY += 32;
+                    offsetY += OFFSET_Y;
                 }
             }
 
             if (tileOrSprinkler) {
-                iv.setFitHeight(64);
+                iv.setFitHeight(HEIGHT);
             } else {
-                iv.setFitHeight(128);
+                iv.setFitHeight(HEIGHT * 2);
             }
-            iv.setFitWidth(64);
+            iv.setFitWidth(HEIGHT);
 
             SnapshotParameters params = new SnapshotParameters();
             params.setFill(javafx.scene.paint.Color.TRANSPARENT);
@@ -77,19 +96,17 @@ public class ShopButton extends Button {
             event.consume();
         });
 
-        setOnMouseReleased(event -> {
-            setEffect(null);
-        });
+        setOnMouseReleased(event -> setEffect(null));
 
-        setOnMouseEntered(event -> {
-            setEffect(new DropShadow());
-        });
+        setOnMouseEntered(event -> setEffect(new DropShadow()));
 
-        setOnMouseExited(event -> {
-            setEffect(null);
-        });
+        setOnMouseExited(event -> setEffect(null));
     }
-
+    /**
+     * Returns the image of the button.
+     *
+     * @return the image of the button
+     */
     public Image getImage() {
         return itemImg;
     }
