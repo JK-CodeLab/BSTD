e89816cdd581aa9f909c72d865ec9b46ca7d4ccb
Joseph Chun <83483639+Mrrm2@users.noreply.github.com>
2023-04-08 22:28:41 -0700

    Add javadoc

--- src/main/java/com/jk/bstd/ui/GameGrid.java
+++ src/main/java/com/jk/bstd/ui/GameGrid.java
@@ -4,33 +4,35 @@ import com.jk.bstd.entities.Entity;
 import com.jk.bstd.entities.Point;
 import com.jk.bstd.entities.Tile;
 import com.jk.bstd.entities.Tower;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
+/**
+ * A class to represent the game grid.
+ *
+ * @author Joseph Chun, Kira Yoon
+ * @version 1.0
+ */
 public class GameGrid {
-    private final GridPane gridPane;
     private static final int ROWS = 7;
     private static final int COLUMNS = 14;
     private static final int GRID_SIZE = 64;
+    private static final int GRID_SIZE_MULTIPLIER = 4;
+    private final GridPane gridPane;
     private ArrayList<Tile> placedTiles = new ArrayList<>();
     private ArrayList<Tower> placedTowers = new ArrayList<>();
-
-
+    /**
+     * Constructor for GameGrid.
+     */
     public GameGrid() {
         gridPane = new GridPane();
-
-        // TODO: Remove this line (for testing purposes)
         gridPane.setGridLinesVisible(true);
 
         gridPane.setPrefSize(COLUMNS * GRID_SIZE, ROWS * GRID_SIZE);
-        gridPane.setLayoutY(GRID_SIZE * 4);
+        gridPane.setLayoutY(GRID_SIZE * GRID_SIZE_MULTIPLIER);
         gridPane.setLayoutX(GRID_SIZE);
 
         initGrid();
@@ -48,11 +50,6 @@ public class GameGrid {
         }
         setFirstTile();
     }
-
-    public int getNumTiles() {
-        return placedTiles.size();
-    }
-
     private void setFirstTile() {
         Tile firstTile = new Tile(new Point(1, 0));
         placedTiles.add(firstTile);
@@ -95,35 +92,65 @@ public class GameGrid {
 //        placedTiles.add(new Tile(new Point(11, 0)));
 //        placedTiles.add(new Tile(new Point(12, 0)));
     }
+
+    /**
+     * Returns an ArrayList of placed towers.
+     *
+     * @return an ArrayList of placed towers
+     */
     public ArrayList<Tower> getPlacedTowers() {
         return placedTowers;
     }
-
-    public void setPlacedTowers(ArrayList<Tower> placedTowers) {
+    /**
+     * Sets the placed towers.
+     *
+     * @param placedTowers the placed towers
+     */
+    public void setPlacedTowers(final ArrayList<Tower> placedTowers) {
         this.placedTowers = placedTowers;
     }
-
+    /**
+     * Returns an ArrayList of placed tiles.
+     *
+     * @return an ArrayList of placed tiles
+     */
     public ArrayList<Tile> getPlacedTiles() {
         return placedTiles;
     }
-
-    public void setPlacedTiles(ArrayList<Tile> placedTiles) {
+    /**
+     * Sets the ArrayList of placed tiles.
+     *
+     * @param placedTiles the ArrayList of placed tiles
+     */
+    public void setPlacedTiles(final ArrayList<Tile> placedTiles) {
         this.placedTiles = placedTiles;
     }
-
+    /**
+     * Returns the grid pane.
+     *
+     * @return the grid pane
+     */
     public GridPane getGridPane() {
         return gridPane;
     }
-
-    public void addEntity(Entity entity) {
+    /**
+     *  Adds entities into their respective ArrayList.
+     *
+     * @param entity the entity to be added
+     */
+    public void addEntity(final Entity entity) {
         if (entity instanceof Tile) {
             placedTiles.add((Tile) entity);
         } else if (entity instanceof Tower) {
             placedTowers.add((Tower) entity);
         }
     }
-
-    public void removeEntity(Entity entity) {
+    /**
+     * Removes entities from their respective ArrayList.
+     *
+     * @param entity the entity to be removed
+     */
+    public void removeEntity(final Entity entity) {
         if (entity instanceof Tile) {
             placedTiles.remove(entity);
         } else if (entity instanceof Tower) {
